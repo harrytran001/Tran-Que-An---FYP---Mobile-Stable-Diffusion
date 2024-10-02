034b7c79de077b25a4fe2a1c940ebf042dea4cfc
Harry <141598639+harrytran001@users.noreply.github.com>
2024-10-02 17:26:28 +0800

    fix time per step

--- python_experiment/helpers.py
+++ python_experiment/helpers.py
@@ -1,5 +1,6 @@
 import torch
 
+from time import time
 from diffusers import StableDiffusionPipeline
 from typing import Callable, List, Optional, Union
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
@@ -134,7 +135,6 @@ class CustomStableDiffusionPipeline(StableDiffusionPipeline):
         # 4. Prepare timesteps
         self.scheduler.set_timesteps(num_inference_steps, device=device)
         timesteps = self.scheduler.timesteps
-
         # 5. Prepare latent variables
         num_channels_latents = self.unet.in_channels
         latents = self.prepare_latents(
@@ -150,42 +150,41 @@ class CustomStableDiffusionPipeline(StableDiffusionPipeline):
 
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
-
         # 7. Denoising loop
         num_warmup_steps = len(timesteps) - num_inference_steps * self.scheduler.order
-        with self.progress_bar(total=num_inference_steps) as progress_bar:
-            for i, t in enumerate(timesteps):  # 10
-                latent_model_input = (
-                    torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+        for i, t in enumerate(timesteps):  # 10
+            start_time = time()
+            latent_model_input = (
+                torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+            )
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+
+            # predict the noise residual
+            noise_pred = self.unet(
+                latent_model_input, t, encoder_hidden_states=prompt_embeds
+            ).sample
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (
+                    noise_pred_text - noise_pred_uncond
                 )
-                latent_model_input = self.scheduler.scale_model_input(
-                    latent_model_input, t
-                )
 
-                # predict the noise residual
-                noise_pred = self.unet(
-                    latent_model_input, t, encoder_hidden_states=prompt_embeds
-                ).sample
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = self.scheduler.step(
+                noise_pred, t, latents, **extra_step_kwargs
+            ).prev_sample
 
-                # perform guidance
-                if do_classifier_free_guidance:
-                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred = noise_pred_uncond + guidance_scale * (
-                        noise_pred_text - noise_pred_uncond
-                    )
+            # call the callback, if provided
+            if i == len(timesteps) - 1 or (
+                (i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0
+            ):
 
-                # compute the previous noisy sample x_t -> x_t-1
-                latents = self.scheduler.step(
-                    noise_pred, t, latents, **extra_step_kwargs
-                ).prev_sample
-
-                # call the callback, if provided
-                if i == len(timesteps) - 1 or (
-                    (i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0
-                ):
-                    progress_bar.update()
-                    if callback is not None and i % callback_steps == 0:
-                        callback(i, t, latents)
+                if callback is not None and i % callback_steps == 0:
+                    callback(i, t, latents)
+            if i + 1 <= num_inference_steps:
+                print(f"Step {i + 1}/{num_inference_steps}: {time() - start_time}")
 
         # 8. Post-processing
         image = self.decode_latents(latents)
